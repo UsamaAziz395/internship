3ef8191e540ef453f0fb67b25f3c938f0595d120
Usama Aziz <usamaaziz395@gmail.com>
2026-09-14 15:29:56 +0500

    check deply

--- src/Components/Partners/Partners.jsx
+++ src/Components/Partners/Partners.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-// import HexaLogo from '../../assets/Gallery/HexaLogo.png'
 
 import HexaLogo from "../../assets/HexaLogo.png"
-import Techlogo from '../../assets/Techlogo.png'
+import Techlogo from '../../assets/TechLogo.png'
 import RealEstatelogo from '../../assets/RealEstatelogo.png'
 import Monogram from '../../assets/Monogram.png'
 
